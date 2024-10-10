fca7607e176e3a0ad15039779b5e36b1f6e735ef
m-lito13 <mikelit13@gmail.com>
2024-10-10 23:04:36 +0300

    More changes

--- pery_ui/src/componets/articlecontentpage.tsx
+++ pery_ui/src/componets/articlecontentpage.tsx
@@ -1,27 +1,42 @@
-import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
+import { Button, createTheme, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { CommonPageProps, OnBoardPropsWrapped } from "./commonpageprops";
 import { TokenRequestFields } from "./tokenrequest";
 import { ArticleContentProps } from "./articlecontentprops";
 
+const theme = createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          contained: {
+            padding: "10px 20px",
+            fontSize: "16px",
+            fontWeight: "500"
+          }
+        }
+      }
+    }
+  });
+
 
 function ArticleContentPage(props: ArticleContentProps) {
+
+
+
     const [languageValueFromControl, setValue] = useState('en');
     function handleClick() {
         ;
     }
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue((event.target as HTMLInputElement).value);
-    };
-
     return (
         <div className="App">
-            <FormControl>
-                <FormLabel id="demo-controlled-radio-buttons-group">Article Content</FormLabel>
-                <TextField value={props.articleContent}></TextField>
-            </FormControl>
-            <Button variant="contained" onClick={() => handleClick()}>Continue</Button>
+                <label>Article content</label>
+                <Typography>
+                    <pre style={{ fontFamily: 'Gazpacho' , textAlign : "left"}}>
+                        {props.articleContent}
+                    </pre>
+                </Typography>
+            <Button variant="contained" onClick={() => handleClick()}>Start Over</Button>
         </div>
     );
 }
--- pery_ui/src/componets/onboardpage1.tsx
+++ pery_ui/src/componets/onboardpage1.tsx
@@ -1,14 +1,37 @@
-import { Button, TextField } from "@mui/material";
+import { Box, Button, Container, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 import { CommonPageProps, OnBoardPropsWrapped } from "./commonpageprops";
 import { TokenRequestFields } from "./tokenrequest";
+import Grid from '@mui/material/Grid2';
+import { createTheme, styled, ThemeProvider } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
+
+//import '../styles/common.css';
+const theme = createTheme({
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                contained: {
+                    padding: "10px 20px",
+                    fontSize: "16px",
+                    fontWeight: "500",
+                    backgroundColor: "#7D6DF6",
+                    border: "1px",
+                    borderRadius: "8px",
+                    width: "169px",
+                    height: "49px"
+                }
+            }
+        }
+    }
+});
 
 function OnBoardPage1(props: OnBoardPropsWrapped) {
     let [tokenRequest, setTokenRequest] = useState({});
     let [userNameFromText, setUserName] = useState('');
 
-    async function handleClick() {
+    function handleClick() {
         let currentRequest: TokenRequestFields = props.commonPageProps.tokenRequest;
         let updatedRequest = { userName: userNameFromText, language: currentRequest.language } as TokenRequestFields;
         props.commonPageProps.updateRequest(updatedRequest);
@@ -23,10 +46,27 @@ function OnBoardPage1(props: OnBoardPropsWrapped) {
     }
 
     return (
-        <div className="App">
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={handleUserNameChanged} />
-            <Button variant="contained" onClick={() => handleClick()}>Continue</Button>
-        </div>
+        
+            <ThemeProvider theme={theme}>
+                <Grid container direction ="row" style={{ display: 'flex' }}spacing={2} wrap='nowrap' sx={{ width: '100%' }}>
+                    <Grid>
+                        <Box height='100%'>
+                            <Typography component="div" style={{ backgroundColor: '#cfe8fc' }}>
+                                Welcome to Pery
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid size={5}>
+                        <div>
+                            <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={handleUserNameChanged} />
+
+                            <Button variant="contained" onClick={() => handleClick()}>Continue</Button>
+                        </div>
+                    </Grid>
+                </Grid>
+            </ThemeProvider> 
+            
+
     );
 }
 
